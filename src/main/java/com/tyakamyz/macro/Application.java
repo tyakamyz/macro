e5ec60d3f4d3080043171481ce4fd74fe78bb97f
tyakamyz <tyakamyz@naver.com>
2023-01-01 14:16:27 +0900

    feat: 네이버쇼핑 테스트 완료

---
+++ src/main/java/com/tyakamyz/macro/Application.java
@@ -0,0 +1,23 @@
+package com.tyakamyz.macro;
+
+import com.tyakamyz.macro.domain.NaverShopping;
+
+public class Application {
+
+    private static final String WEB_DRIVER_ID = "webdriver.chrome.driver"; // 드라이버 ID
+    private static final String WEB_DRIVER_PATH = "D:\\IdeaProjects\\macro\\src\\main\\resources\\chromedriver.exe"; // 드라이버 경로
+
+    static {
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+    }
+
+    public static void main(String[] args) {
+        String naverId = "";
+        String naverPw = "";
+        String targetUrl = "https://search.naver.com/p/crd/rd?m=1&px=602&py=228&sx=602&sy=228&p=h3trYsp0J1sssOGF%2FpGssssssyh-360131&q=%EC%9D%B5%EC%82%B0%EB%86%8D%ED%98%91%EB%96%A1%EB%B0%A9%EC%95%97%EA%B0%84&ie=utf8&rev=1&ssc=tab.nx.all&f=nexearch&w=nexearch&s=U52lnWse%2Bfblw0a%2FgaMnrw%3D%3D&time=1668312100231&abt=%5B%7B%22eid%22%3A%22SBR1%22%2C%22vid%22%3A%22490%22%7D%5D&u=https%3A%2F%2Fcr3.shopping.naver.com%2Fbridge%2FsearchGate%3Fquery%3D%25EC%259D%25B5%25EC%2582%25B0%25EB%2586%258D%25ED%2598%2591%25EB%2596%25A1%25EB%25B0%25A9%25EC%2595%2597%25EA%25B0%2584%26bt%3D-1%26nv_mid%3D84959545731%26cat_id%3D50013161%26h%3Df2521c2427fdb1439234a4856a28af0471b95dd0%26t%3DLAETVYX2%26frm%3DNVSCPRO&r=1&i=00000009_0013c7fbc983&a=shp_dui*a.outtit&cr=1";
+        int optionIndex = 1;
+
+        NaverShopping macro = new NaverShopping(naverId, naverPw, targetUrl);
+        macro.start(optionIndex);
+    }
+}
---
+++ src/main/java/com/tyakamyz/macro/domain/NaverShopping.java
@@ -0,0 +1,87 @@
+package com.tyakamyz.macro.domain;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class NaverShopping {
+    private final String id;
+    private final String pw;
+    private final String targetUrl;
+    /**
+     * Chrome Version : 107.0.5304.88
+     * Chrome Version 에 맞는 Driver 설치
+     * https://chromedriver.chromium.org/downloads
+     */
+    private final ChromeDriver driver;
+
+    private static final String NAVER_URL = "https://www.naver.com";
+
+    public NaverShopping(String id, String pw, String targetUrl) {
+        this.id = id;
+        this.pw = pw;
+        this.targetUrl = targetUrl;
+        this.driver = setDriver();
+    }
+
+    public ChromeDriver setDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        return new ChromeDriver(options);
+    }
+
+    /**
+     * 구매할 옵션 번호
+     * ex) 1번 옵션 -> optionIndex = 1
+     */
+    public void start(int optionIndex){
+        start();
+        login();
+        while(!purchase(optionIndex)){
+        }
+    }
+
+    private void start() {
+        driver.get(NAVER_URL);
+        WebElement loginBtn = driver.findElement(By.className("link_login"));
+        loginBtn.click();
+    }
+
+    private void login(){
+        // ID/PW script로 넣기 (captcha 우회)
+        String script =  "(function execute(){document.querySelector('#id').value = '" + id + "';document.querySelector('#pw').value = '" + pw + "';})();";
+        JavascriptExecutor javascriptExecutor = driver;
+        javascriptExecutor.executeScript(script);
+
+        // 로그인 버튼 클릭
+        driver.findElement(By.id("log.login")).click();
+
+        // 등록 버튼 클릭
+        //driver.findElement(By.id("new.save")).click();
+    }
+
+    private boolean purchase(int optionIndex) {
+        try {
+            driver.get(targetUrl);
+            // 상품 선택 버튼
+            driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div[2]/fieldset/div[5]/div/a")).click();
+            // 옵션 선택
+            driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div[2]/fieldset/div[5]/div/ul/li["+ optionIndex +"]/a")).click();
+            // 구매 클릭
+            driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div[2]/fieldset/div[8]/div[1]/div/a")).click();
+            Thread.sleep(1000);
+            // 일반 결제 클릭
+            driver.findElement(By.xpath("//*[@id=\"chargePointScrollArea\"]/div[1]/ul[1]/li[4]/div[1]/span[1]/span")).click();
+            // 나중에 결제 클릭
+            driver.findElement(By.xpath("//*[@id=\"chargePointScrollArea\"]/div[1]/ul[1]/li[4]/ul/li[3]/span[1]/span")).click();
+            // 결제 버튼 클릭
+            driver.findElement(By.xpath("//*[@id=\"orderForm\"]/div/div[7]/button")).click();
+        }catch (Exception e){
+            System.out.println("주문 실패");
+            return false;
+        }
+        return true;
+    }
+}
